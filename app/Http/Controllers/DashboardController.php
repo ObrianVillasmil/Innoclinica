5a03c4caa1789aba9f2811d932c9e541df326612
Obrian Villasmil <villasmilobrian@gmail.com>
2020-03-02 14:23:16 -0500

    updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,14 +14,16 @@ class DashboardController extends Controller
         if($dataTratamientos->count() == 0)
             flash('No existen tratamientos disponibles')->error();
 
-        getParty(session('party_id'))->party_role->role_type->role_type_id === "MEDICO_USUARIO"
+        $party = getParty(session('party_id'));
+
+            (isset($party->party_role) && $party->party_role->role_type->role_type_id === "MEDICO_USUARIO")
             ? $tratamientos = Tratamiento::where([['tratamiento.estado',1],['ts.id_doctor',session('party_id')]])->join('tratamiento_solicitado as ts','tratamiento.id_tratamiento','ts.id_tratamiento')->orderBy('nombre_tratamiento','asc')->get()
             : $tratamientos = Tratamiento::where('estado',1)->orderBy('nombre_tratamiento','asc')->get();
 
         return view('layouts.indicadores.dashboard',[
             'url' => $request->path(),
             'titulo' => ['titulo'=>'Página de inicio','sub_titulo'=>'Dashboard'],
-            'usuario' => getParty(session('party_id')),
+            'usuario' => $party,
             'tratamientos' => $tratamientos
         ]);
     }
