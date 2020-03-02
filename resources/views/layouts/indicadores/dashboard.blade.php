5b2f15bd3d462c78fcc85abbcf19d4972a5ef98f
Obrian Villasmil <villasmilobrian@gmail.com>
2020-03-02 14:57:28 -0500

    updates

--- resources/views/layouts/indicadores/dashboard.blade.php
+++ resources/views/layouts/indicadores/dashboard.blade.php
@@ -107,38 +107,42 @@
     <div class="col-md-12">
         <div class="row">
             @if(isset($usuario->party_role))
-                @if($tratamientos->count() > 0)
-                    @foreach($tratamientos as $t)
-                        @php
-                            $tratamientoSolicitado = getTratamientoSolicitado($t->id_tratamiento,isset($t->party_id) ? $t->party_id : session('party_id'));
-                        @endphp
-                        <div class="col-md-3 col-lg-3 text-center" style="height: 300px;{{$t->imagen === null ? "border:1px solid" : "" }}" title="{{$t->nombre_tratamiento}}">
-                            <a href="{{url('tratamientos_clientes/proceso_tratamiento',[$t->id_tratamiento,isset($tratamientoSolicitado->id_tratamiento_solicitado) ? $tratamientoSolicitado->id_tratamiento_solicitado : null,$t->party_id])}}" style="{{ $t->imagen === null ? "margin: 133px auto" : ""}};text-align: center;width: 100%;">
-                                <div>
-                                    {!! $t->imagen === null ? $t->nombre_tratamiento : "<img class='img-fluid' src='/storage/img_tratamientos/".$t->imagen."'>" !!}
+                @if(isset($usuario->identification->id_value))
+                    @if($tratamientos->count() > 0)
+                        @foreach($tratamientos as $t)
+                            @php
+                                $tratamientoSolicitado = getTratamientoSolicitado($t->id_tratamiento,isset($t->party_id) ? $t->party_id : session('party_id'));
+                            @endphp
+                            <div class="col-md-3 col-lg-3 text-center" style="height: 300px;{{$t->imagen === null ? "border:1px solid" : "" }}" title="{{$t->nombre_tratamiento}}">
+                                <a href="{{url('tratamientos_clientes/proceso_tratamiento',[$t->id_tratamiento,isset($tratamientoSolicitado->id_tratamiento_solicitado) ? $tratamientoSolicitado->id_tratamiento_solicitado : null,$t->party_id])}}" style="{{ $t->imagen === null ? "margin: 133px auto" : ""}};text-align: center;width: 100%;">
+                                    <div>
+                                        {!! $t->imagen === null ? $t->nombre_tratamiento : "<img class='img-fluid' src='/storage/img_tratamientos/".$t->imagen."'>" !!}
+                                    </div>
+                                </a>
+                                <div class="text-success" style="margin-top: 10px">
+                                    @if($usuario->party_role->role_type->role_type_id === "END_USER_CUSTOMER" || $usuario->party_role->role_type->role_type_id === "ADMIN" || getParty(session('party_id'))->party_role->role_type->role_type_id == "REPRESENTANTE_LEGAL" &&  getTratamientoSolicitado($t->id_tratamiento,session('party_id')) != null)
+                                        <i class="fa fa-check-square-o"></i> Solicitado
+                                    @endif
+                                    @if(getParty(session('party_id'))->party_role->role_type->role_type_id === "MEDICO_USUARIO")
+                                        Solicitado por {{getParty($t->party_id)->person->first_name ." ". getParty($t->party_id)->person->last_name}}
+                                    @endif
                                 </div>
-                            </a>
-                            <div class="text-success" style="margin-top: 10px">
-                                @if($usuario->party_role->role_type->role_type_id === "END_USER_CUSTOMER" || $usuario->party_role->role_type->role_type_id === "ADMIN" || getParty(session('party_id'))->party_role->role_type->role_type_id == "REPRESENTANTE_LEGAL" &&  getTratamientoSolicitado($t->id_tratamiento,session('party_id')) != null)
-                                    <i class="fa fa-check-square-o"></i> Solicitado
-                                @endif
-                                @if(getParty(session('party_id'))->party_role->role_type->role_type_id === "MEDICO_USUARIO")
-                                    Solicitado por {{getParty($t->party_id)->person->first_name ." ". getParty($t->party_id)->person->last_name}}
+                            </div>
+                        @endforeach
+                    @else
+                        <div class="col-md-12">
+                            <div class="alert alert-info text-center" role="alert">
+                                @if(getParty(session('party_id'))->party_role->role_type->role_type_id === "ADMIN")
+                                    No hay tratamientos solcitados por el momento
+                                @else
+                                    No hay tratamientos disponibles por el momento
                                 @endif
                             </div>
                         </div>
-                    @endforeach
-                @else
-                    <div class="col-md-12">
-                        <div class="alert alert-info text-center" role="alert">
-                            @if(getParty(session('party_id'))->party_role->role_type->role_type_id === "ADMIN")
-                                No hay tratamientos solcitados por el momento
-                            @else
-                                No hay tratamientos disponibles por el momento
-                            @endif
-                        </div>
-                    </div>
-                @endif
+                    @endif
+                    @else
+                        <div class="alert alert-info text-center">Debe completar sus datos en el sistema, para poder realizar acciones en el sistema, dirijase a la seccion Perfil en el menú de usuario</div>
+                    @endif
                 @else
                 <div class="alert alert-info text-center">Debe seleccionar su rol de usuario para poder realizar acciones en el sistema</div>
             @endif
